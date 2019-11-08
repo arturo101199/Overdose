2f5a739a3e7d0bec4657c55b24917454d4e6482e
arturo101199 <arturo101199@hotmail.es>
2019-11-08 12:02:10 +0100

    Añadido Camera Offset

--- Overdose/Assets/Scripts/camara.cs
+++ Overdose/Assets/Scripts/camara.cs
@@ -5,28 +5,45 @@ using Cinemachine;
 
 public class camara : MonoBehaviour
 {
-    public float cameraSmooth;
-    public CinemachineVirtualCamera camera;
+    public float cameraSmoothX;
+    public float cameraSmoothY;
+    CinemachineFramingTransposer cameraTransposer;
+    CinemachineCameraOffset cameraOffset;
 
-    float screen = 0.5f;
+    float screenXOffset = 0.5f;
+    float screenYOffset = 0;
+    float currentCameraY;
     // Start is called before the first frame update
-
+    void Awake()
+    {
+        cameraTransposer = GetComponent<CinemachineVirtualCamera>().GetCinemachineComponent<CinemachineFramingTransposer>();
+        cameraOffset = GetComponent<CinemachineCameraOffset>();
+    }
     // Update is called once per frame
     void FixedUpdate()
     {
         if (Input.GetAxisRaw("Horizontal") == -1)
         {
-            screen = Mathf.Clamp(screen + cameraSmooth * Time.fixedDeltaTime, 0.3f, 0.8f);
-            Mathf.Clamp(screen, 0.3f, 0.8f);
-            camera.GetCinemachineComponent<CinemachineFramingTransposer>().m_ScreenX = screen;
+            screenXOffset = Mathf.Clamp(screenXOffset + cameraSmoothX * Time.fixedDeltaTime, 0.3f, 0.7f);
+            cameraTransposer.m_ScreenX = screenXOffset;
 
         }
         else if (Input.GetAxisRaw("Horizontal") == 1)
         {
-            screen = Mathf.Clamp(screen - cameraSmooth * Time.fixedDeltaTime, 0.3f, 0.8f);
-            Mathf.Clamp(screen, 0.3f, 0.8f);
-            camera.GetCinemachineComponent<CinemachineFramingTransposer>().m_ScreenX = screen;
+            screenXOffset = Mathf.Clamp(screenXOffset - cameraSmoothX * Time.fixedDeltaTime, 0.3f, 0.7f);
+            cameraTransposer.m_ScreenX = screenXOffset;
 
+        }
+        else if (Input.GetKey("w"))
+        {
+            Debug.Log(currentCameraY + " " + transform.position.y);
+            if(currentCameraY != transform.position.y)
+            {
+                currentCameraY = transform.position.y;
+                screenYOffset = screenYOffset + cameraSmoothY * Time.fixedDeltaTime;
+                cameraOffset.m_Offset = Vector3.up * screenYOffset;
+            }
+                
         }
     }
 }
