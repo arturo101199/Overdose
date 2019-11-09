437f4ff8d52b9b8578f7af0d33dae2f06a8cf8ad
arturo101199 <arturo101199@hotmail.es>
2019-11-09 12:38:12 +0100

    intento de movimiento vertical de camara

--- Overdose/Assets/Scripts/camara.cs
+++ Overdose/Assets/Scripts/camara.cs
@@ -2,48 +2,62 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using DG.Tweening;
 
 public class camara : MonoBehaviour
 {
     public float cameraSmoothX;
     public float cameraSmoothY;
     CinemachineFramingTransposer cameraTransposer;
-    CinemachineCameraOffset cameraOffset;
+    CinemachineVirtualCamera camera;
+    bool desplazando = false;
 
     float screenXOffset = 0.5f;
-    float screenYOffset = 0;
-    float currentCameraY;
+    public float tiempoRecuperacionCamara;
+   
     // Start is called before the first frame update
     void Awake()
     {
-        cameraTransposer = GetComponent<CinemachineVirtualCamera>().GetCinemachineComponent<CinemachineFramingTransposer>();
-        cameraOffset = GetComponent<CinemachineCameraOffset>();
+        camera = GetComponent<CinemachineVirtualCamera>();
+        cameraTransposer = camera.GetCinemachineComponent<CinemachineFramingTransposer>();
     }
     // Update is called once per frame
     void FixedUpdate()
     {
         if (Input.GetAxisRaw("Horizontal") == -1)
         {
+            if (desplazando)
+            {
+                recuperarCamara();
+            }
             screenXOffset = Mathf.Clamp(screenXOffset + cameraSmoothX * Time.fixedDeltaTime, 0.3f, 0.7f);
             cameraTransposer.m_ScreenX = screenXOffset;
 
         }
         else if (Input.GetAxisRaw("Horizontal") == 1)
         {
+            if (desplazando)
+            {
+                recuperarCamara();
+            }
             screenXOffset = Mathf.Clamp(screenXOffset - cameraSmoothX * Time.fixedDeltaTime, 0.3f, 0.7f);
             cameraTransposer.m_ScreenX = screenXOffset;
-
         }
         else if (Input.GetKey("w"))
         {
-            Debug.Log(currentCameraY + " " + transform.position.y);
-            if(currentCameraY != transform.position.y)
-            {
-                currentCameraY = transform.position.y;
-                screenYOffset = screenYOffset + cameraSmoothY * Time.fixedDeltaTime;
-                cameraOffset.m_Offset = Vector3.up * screenYOffset;
-            }
-                
+            desplazando = true;
+            cameraTransposer.m_ScreenY += 0.02f;
         }
+        else if (Input.GetKeyUp("w"))
+        {
+            recuperarCamara();
+            desplazando = false;
+            //cameraTransposer.m_ScreenY = 0.5f;
+        }
+    }
+
+    void recuperarCamara()
+    {
+        DOTween.To(() => cameraTransposer.m_ScreenY, x => cameraTransposer.m_ScreenY = x, 0.5f, tiempoRecuperacionCamara);
     }
 }
