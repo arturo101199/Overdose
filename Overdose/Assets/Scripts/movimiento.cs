3879bc758c1802a2ab2d1915b85e417f4813903d
BrunoCetraro <al376455@uji.es>
2019-11-12 02:09:47 +0100

    salto y movimiento mal
    problemas con addforce que no me hace una mierda independientemente de cómo lo haga need help.

--- Overdose/Assets/Scripts/movimiento.cs
+++ Overdose/Assets/Scripts/movimiento.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class movimiento : MonoBehaviour
 {
-
+    private Vector2 jump;
     private Rigidbody2D rb2d;
     public float speed;
+   
+    public float jumpForce;
+    bool isJumping;
 
     private void Start()
     {
@@ -21,7 +24,22 @@ public class movimiento : MonoBehaviour
         float moverHorizontal = Input.GetAxisRaw ("Horizontal");
         // float moverVertical = Input.GetAxisRaw ("Vertical");
         Vector2 movement = new Vector2(moverHorizontal * speed, 0);//moverVertical*speed);
-        rb2d.velocity= movement;
+        rb2d.velocity = movement;
+        
+        Jump();
+        
 
     }
-}	
+    void Jump() {
+        if (Input.GetKeyDown(KeyCode.Space)&& !isJumping)
+        {
+            isJumping = true; // variable para saber cuando saltar, la cosa es actualizar cuando haya colliders
+            Debug.Log("sida");
+            isJumping = true;
+            //rb2d.AddForce(new Vector2(0, jumpForce));
+            rb2d.velocity = Vector2.up*jumpForce;
+;
+        }
+        else isJumping = false;
+    }
+}
---
+++ Overdose/Assets/salto.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class salto : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    public int height;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        bool salto = Input.GetKeyDown(KeyCode.Space);
+        if (salto) {
+            rb2d.AddForce(height * Vector2.up);
+        }
+        
+    }
+}
