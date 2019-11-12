ae7ee08df2ddc25f00554453980569337b8341e7
arturo101199 <arturo101199@hotmail.es>
2019-11-12 11:10:18 +0100

    aplicacion de movimiento de antonio aplicada

---
+++ Overdose/Assets/animacion.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animacion : MonoBehaviour
+{
+    Animator animator;
+    bool derecha = true;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxisRaw("Horizontal") == 1)
+        {
+            animator.SetBool("moviendo", true);
+            if (!derecha)
+            {
+                derecha = true;
+                transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+            }
+        }
+        else if(Input.GetAxisRaw("Horizontal") == -1)
+        {
+            animator.SetBool("moviendo", true);
+            if (derecha)
+            {
+                derecha = false;
+                transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+            }
+        }
+        else
+        {
+            animator.SetBool("moviendo", false);
+        }
+    }
+}
