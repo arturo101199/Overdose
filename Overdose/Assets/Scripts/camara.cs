afe61434f0f7905d7e4a5009a589f4afb386df9b
arturo101199 <arturo101199@hotmail.es>
2019-11-16 16:31:57 +0100

    Archivos recuperados

--- Overdose/Assets/Scripts/camara.cs
+++ Overdose/Assets/Scripts/camara.cs
@@ -2,31 +2,75 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using DG.Tweening;
 
 public class camara : MonoBehaviour
 {
-    public float cameraSmooth;
-    public CinemachineVirtualCamera camera;
+    public float cameraSmoothX;
+    public float cameraSmoothY;
+    CinemachineFramingTransposer cameraTransposer;
+    CinemachineVirtualCamera camera;
+    CinemachineConfiner cameraConfiner;
 
-    float screen = 0.5f;
-    // Start is called before the first frame update
+    bool desplazando;
+    bool techo;
 
-    // Update is called once per frame
-    void FixedUpdate()
+    float screenXOffset = 0.5f;
+    public float tiempoRecuperacionCamara;
+   
+    void Awake()
+    {
+        camera = GetComponent<CinemachineVirtualCamera>();
+        cameraTransposer = camera.GetCinemachineComponent<CinemachineFramingTransposer>();
+        cameraConfiner = GetComponent<CinemachineConfiner>();
+    }
+
+    void Update()
     {
         if (Input.GetAxisRaw("Horizontal") == -1)
         {
-            screen = Mathf.Clamp(screen + cameraSmooth * Time.fixedDeltaTime, 0.3f, 0.8f);
-            Mathf.Clamp(screen, 0.3f, 0.8f);
-            camera.GetCinemachineComponent<CinemachineFramingTransposer>().m_ScreenX = screen;
+            if (desplazando)
+            {
+                recuperarCamara();
+            }
+            screenXOffset = Mathf.Clamp(screenXOffset + cameraSmoothX * Time.deltaTime, 0.3f, 0.7f);
+            cameraTransposer.m_ScreenX = screenXOffset;
 
         }
         else if (Input.GetAxisRaw("Horizontal") == 1)
         {
-            screen = Mathf.Clamp(screen - cameraSmooth * Time.fixedDeltaTime, 0.3f, 0.8f);
-            Mathf.Clamp(screen, 0.3f, 0.8f);
-            camera.GetCinemachineComponent<CinemachineFramingTransposer>().m_ScreenX = screen;
+            if (desplazando)
+            {
+                recuperarCamara();
+            }
+            screenXOffset = Mathf.Clamp(screenXOffset - cameraSmoothX * Time.deltaTime, 0.3f, 0.7f);
+            cameraTransposer.m_ScreenX = screenXOffset;
+        }
+        else if (Input.GetKey("w"))
+        {
+            desplazando = true;
+            if (!techo)
+            {
+               cameraTransposer.m_ScreenY += cameraSmoothY * Time.deltaTime;
+            }
 
+            if (cameraConfiner.CameraWasDisplaced(camera))
+            {
+                techo = true;
+                cameraTransposer.m_ScreenY -= 0.005f;
+            }
+        }
+        else if (Input.GetKeyUp("w"))
+        {
+            recuperarCamara();
+            desplazando = false;
+            techo = false;
         }
     }
+
+    void recuperarCamara()
+    {
+        DOTween.To(() => cameraTransposer.m_ScreenY, x => cameraTransposer.m_ScreenY = x, 0.5f, tiempoRecuperacionCamara);
+    }
+
 }
