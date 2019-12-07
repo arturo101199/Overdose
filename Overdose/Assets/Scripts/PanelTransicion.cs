bdbf58aaa8c150aef32326ba2b7732cb965039bb
arturo101199 <arturo101199@hotmail.es>
2019-12-07 16:19:41 +0100

    Menú principal implementado

---
+++ Overdose/Assets/Scripts/PanelTransicion.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelTransicion : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("DesactivarTransicion", 2);
+    }
+
+    void DesactivarTransicion()
+    {
+        gameObject.SetActive(false);
+    }
+
+}
