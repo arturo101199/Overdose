99cb287cf135ac850f376688b280e256a8beecda
Carrasco <mferrercarrasco@gmail.com>
2020-01-28 11:19:39 +0100

    Interfaz Nueva
    A veces va un poco mal, pero estoy hasta la polla, mientras se vea en el trailer vamos bien

--- Overdose/Assets/Scripts/Interaccion.cs
+++ Overdose/Assets/Scripts/Interaccion.cs
@@ -6,85 +6,31 @@ using System.Collections.Generic;
 
 public class Interaccion : MonoBehaviour
 {
-    //diálogos
+    public string personaje;
     public string npcTextMessage = "Soy un NPC"; //Dialogo
     public string popUpBoxMessage = "Presiona E"; //menasje de la caja de interacción
-    public Rect popupBox = new Rect(0.25f, 0.75f, 0.5f, 0.1f); //Tamaño y posición de la caja que aparece para interactuar
-    public Rect messageBox = new Rect(0.1f, 0.09f, 0.8f, 0.2f); //Tamaño y posición de la caja de dialogo
-    public Transform player; //Antonio
-    public float minDistance = 1f; //Distancia para activar la interacción
 
-    private bool inRange = false;
-    private bool showText = false;
-
-    public bool terminal;
+    public bool terminal = false;
     public AudioClip hackear;
-    private AudioSource source;
+    public AudioSource source;
     public Puerta puerta_Hackeable;
 
-    public bool puerta;
+    public bool puerta = false;
+
 
     void Start()
     {
-
+        if (transform.tag == "Puerta") puerta = true;
+        else if (transform.tag == "terminal") terminal = true;
         source = GetComponent<AudioSource>();
 
-        /*style = new GUIStyle();
-        style.alignment = TextAnchor.MiddleCenter;
-        style.fontSize = 18;*/
-
-        //mensaje de error si Antonio no está asignado como player en el inspector
-        if (player == null)
-        {
-            Debug.LogError("Variable 'player' not set up on " + gameObject.name + ". Disabling the associated script.");
-            this.enabled = false;
-        }
     }
 
     void Update()
     {
-        if (Math.Abs(transform.position.x - player.position.x) <= minDistance
-            && Math.Abs(transform.position.y - player.position.y) <= 10) //Se comprueba la distancia
+        if (gameObject.GetComponent<Puerta>().abierta)
         {
-            //Activar el mensaje de la caja de interacción
-            inRange = true;
-
-            if (Input.GetKeyDown("e"))
-            {
-                if (terminal & !showText)
-                {
-                    source.Stop();
-                    source.PlayOneShot(hackear);
-                    puerta_Hackeable.abierta = true;
-                    terminal = false;
-                    popUpBoxMessage = "";
-                }
-                showText = !showText;
-            }
-        }
-        else
-        {
-            //Los textos desaparecen si Antonio se aleja
-            if (terminal) source.Stop();
-            inRange = false;
-            showText = false;
-            
-        }
-    }
-
-    void OnGUI()
-    {
-        GUI.skin.box.fontSize = 18;
-        GUI.skin.box.alignment = TextAnchor.MiddleCenter;
-
-        if (inRange && !showText && popUpBoxMessage != "")
-        {
-            if (!puerta) GUI.Box(new Rect(Screen.width * popupBox.x, Screen.height * popupBox.y, Screen.width * popupBox.width, Screen.height * popupBox.height), popUpBoxMessage);
-        }
-
-        if (showText && npcTextMessage != "")
-        {
-            if (!puerta || (puerta && !transform.gameObject.GetComponent<Puerta>().abierta)) GUI.Box(new Rect(Screen.width * messageBox.x, Screen.height * messageBox.y, Screen.width * messageBox.width, Screen.height * messageBox.height), npcTextMessage);
+            npcTextMessage = "";
         }
     }
 }
---
+++ Overdose/Assets/Scripts/Interacciones.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Interacciones : MonoBehaviour
+{
+    private bool inRange;
+    private bool showText;
+    private bool unTerminal;
+    private Interaccion objeto;
+
+    public GameObject dBox;
+    public Text dText;
+    public Text dChar;
+
+    public GameObject puBox;
+    public Text puText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<Interaccion>())
+        {
+            inRange = true;
+            objeto = collision.gameObject.GetComponent<Interaccion>();
+
+            if (Input.GetKeyDown("e"))
+            {
+                if (objeto.terminal)
+                {
+                    objeto.source.Stop();
+                    objeto.source.PlayOneShot(objeto.hackear);
+                    objeto.puerta_Hackeable.abierta = true;
+                }
+                showText = !showText;
+            }
+        }
+        /*else
+        {
+            //Los textos desaparecen si Antonio se aleja
+            if (objeto.terminal) objeto.source.Stop();
+            inRange = false;
+            showText = false;
+       
+        }*/
+       
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<Interaccion>())
+        {
+            objeto = collision.gameObject.GetComponent<Interaccion>();
+            if (objeto.terminal)
+            {
+                objeto.source.Stop();
+                objeto.terminal = false;
+                objeto.popUpBoxMessage = "";
+            }
+            inRange = false;
+            showText = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnGUI()
+    {
+        if (inRange && !showText && objeto.popUpBoxMessage != "")
+        {
+
+                puBox.SetActive(true);
+                puText.text = objeto.popUpBoxMessage;
+                dBox.SetActive(false);
+        }
+
+        else if (showText && objeto.npcTextMessage != "")
+        {
+            
+            if (!objeto.puerta || (objeto.puerta && !objeto.gameObject.GetComponent<Puerta>().abierta))
+            {
+                dBox.SetActive(true);
+                dText.text = objeto.npcTextMessage;
+                dChar.text = objeto.personaje;
+                puBox.SetActive(false);
+            }
+        }
+        else
+        {
+            dBox.SetActive(false);
+            puBox.SetActive(false);
+        }
+    }
+}
